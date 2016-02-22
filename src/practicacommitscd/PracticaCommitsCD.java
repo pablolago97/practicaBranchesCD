aef84aaffd425fd9dac2eee7fa448869f0a41c20
plago-bergonpazos <plago-bergonpazos@atanasoff14.danielcastelao.org>
2016-02-22 09:04:40 +0100

    ''take it easy''

--- src/practicacommitscd/PracticaCommitsCD.java
+++ src/practicacommitscd/PracticaCommitsCD.java
@@ -15,7 +15,8 @@ public class PracticaCommitsCD {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        System.out.println("Hello world");
+        System.out.println("take it easy");
     }
     
 }
